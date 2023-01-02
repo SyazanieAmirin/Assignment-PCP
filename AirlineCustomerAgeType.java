062e8363669f7a1e03c97df0f12c8e8bc018e709
Syazanie Amirin <51242108+SyazanieAmirin@users.noreply.github.com>
2023-01-02 14:16:35 +0800

    Update AirlineCustomerAgeType.java

--- AirlineCustomerAgeType.java
+++ AirlineCustomerAgeType.java
@@ -6,22 +6,17 @@ public class AirlineCustomerAgeType {
 	
 	public AirlineCustomerAgeType(int custAge) {
 		
-		airline air = new airline();
-		
 		// Customer age 12
 		if(custAge < 13) {
 			this.additionalPriceBetweenAges = 0.00;
-			air.sumOfFlightPrice += additionalPriceBetweenAges;
 		}
 		
 		if(custAge < 51) {
 			this.additionalPriceBetweenAges = 10.00;
-			air.sumOfFlightPrice += additionalPriceBetweenAges;
 		}
 		
 		if(custAge >= 51) {
 			this.additionalPriceBetweenAges = 5.00;
-			air.sumOfFlightPrice += additionalPriceBetweenAges;
 		}
 	}
 
